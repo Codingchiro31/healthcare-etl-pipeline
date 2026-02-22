be0e705381e49814c80224547c9b0e1697127f8b
Codingchiro31 <139086942+Codingchiro31@users.noreply.github.com>
2026-02-21 21:08:51 -0600

    Add transform.py placeholder

---
+++ transform.py
@@ -0,0 +1,14 @@
+"""
+transform.py
+Transform and clean healthcare data
+"""
+
+import pandas as pd
+
+def transform_data(raw_data):
+  """Clean and transform data"""
+  pass
+
+
+if __name__ == "__main__":
+  tranform_data()
